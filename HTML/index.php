2c4c20a53ba48f40b34a19e7a91a8ec16a7f9f70
Firey-event559 <RaminoHiromiVrca@gmail.com>
2023-03-08 19:41:27 +0100

    update  index.html

--- HTML/index.php
+++ HTML/index.php
@@ -2,33 +2,54 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/main.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../CSS/main.css" />
     <title>Verkennen/ Twitter</title>
 </head>
 
 <body class="aanpassen">
     <main class="twitter-inleiding">
-        <img class="logo-twitter" src="../img/foto-twitter.png" alt="logo-twitter" width="45px" height="45px">
-
+        <a href="index.php">
+            <img class="logo-twitter" src="../img/foto-twitter.png" alt="logo-twitter" width="45px" height="45px" />
+        </a>
         <section class="verkennen">
-            <img src="../img/hastag-logo.png" alt="hastag-logo" width="45px" height="45px">
-            <h1  class="grootte">Verkennen</h1>
+            <img src="../img/hastag-logo.png" alt="hastag-logo" width="45px" height="45px" />
+            <h1 class="grootte">Verkennen</h1>
         </section>
 
-        <section> 
-            <img src="gear.png"
+        <section class="instelling">
+            <img class="gear" src="../img/gear.png" alt="gear" width="45px" height="45px" />
+            <h2 class="grootte">Instellingen</h2>
         </section>
-
-
-
-    
-
-
     </main>
 
+    <div class="border"></div>
+
+    <div class="border2"></div>
+
+    <footer class="onderkant">
+        <div class="button-display-flex">
+            <button id="popup" class="button-footer">Inloggen</button>
+            <button class="button-footer2">Registreren</button>
+        </div>
+        <h1 class="footer-tekst">Mis niets van wat er gebeurt</h1>
+
+        <p class="footer-tekst">
+            Mensen die lid zijn van Twitter, zijn als eersten op de hoogte.
+        </p>
+    </footer>
+
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <img src="../img/foto-twitter.png" alt="foto-twitter" width="45px" height="45px" />
+            <input type="text" name="" id="" placeholder="Email-adress" />
+        </div>
+    </div>
+
+    <script src="../Javascript/main.js"></script>
 </body>
 
 </html>
---
+++ Javascript/main.js
@@ -0,0 +1,24 @@
+
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("popup");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+popup.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
